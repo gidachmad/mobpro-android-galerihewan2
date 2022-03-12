994716eef952e0fe75e99ea84614ef20d92e394e
Gid_Achmad_Ahlul_Fadli <gaaf0809@gmail.com>
2022-03-12 10:55:13 +0700

    membuat layout untuk list

---
+++ app/src/main/java/org/d3if3061/galerihewan/Hewan.kt
@@ -0,0 +1,7 @@
+package org.d3if3061.galerihewan
+
+data class Hewan(
+    val nama: String,
+    val namaLatin: String
+
+)
--- app/src/main/java/org/d3if3061/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if3061/galerihewan/MainActivity.kt
@@ -2,10 +2,28 @@ package org.d3if3061.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("MainActivity", "Jumlah Data " + getData().size)
+    }
+
+    private fun getData(): List<Hewan> {
+        return listOf(
+            Hewan("Angsa", "Cygnus olor"),
+            Hewan("Ayam", "Gallus gallus"),
+            Hewan("Bebek", "Cairina moschata"),
+            Hewan("Domba", "Ovis ammon"),
+            Hewan("Kalkun", "Meleagris gallopavo"),
+            Hewan("Kambing", "Capricornis Sumatrensis"),
+            Hewan("Kelinci", "Oryctolagus cuniculus"),
+            Hewan("Kerbau", "Bubalus bubalis"),
+            Hewan("Kuda", "Equus caballus"),
+            Hewan("Sapi", "Bos taurus"),
+            )
     }
 }
